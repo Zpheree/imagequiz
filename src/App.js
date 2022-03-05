de79f2eabe4f8222f08ca3e75c41d87972625718
Tyriq Zvijer <tyriq811@gmail.com>
2022-03-04 17:18:46 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import Home from './Components/Home';
+import Register from './Components/Register';
+import Login from './Components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/register'>
+          <Register />
+          </Route>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
