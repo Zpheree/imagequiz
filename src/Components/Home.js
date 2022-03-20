e3167a34b649862a1c8a4f356878ff98c3852ac1
Tyriq Zvijer <tyriq811@gmail.com>
2022-03-20 15:23:31 -0700

    ISTA 330 2

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,41 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import flowers from "./data.js";
+
 const Home = () => {
-    return (
-        <div>
-            Hello from the home page
-        </div>
-    );
+  const flower = []
+  let inner = []
+  for (var i = 0; i < flowers.length; i++) {
+    if (i % 4 === 0) {
+        flower.push(inner);
+        inner = []
+    }
+    inner.push(flowers[i]);
+  }
+  inner.push("")
+  flower.push(inner);
+
+  return (
+
+    <Container className="images">
+        {flower.map ((inn, i) => {
+          return (
+            <Row>
+              { inn.map ((image, j) => {
+                return (
+                  <Col className="imgCon" >
+                    <Image className="img" src={image.picture}/>
+                    <p>{image.name}</p>
+                  </Col>
+                );
+              })}
+            </Row>
+          );
+        })}
+
+    </Container>
+  );
 }
+
 
 export default Home;
