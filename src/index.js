76336d7b7ae7dcf3706474f962b20d4a8ade4533
Tyriq Zvijer <tyriq811@gmail.com>
2022-05-01 21:12:00 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './temp_data_repo/reportWebVitals';
 
 ReactDOM.render( <App />, document.getElementById('root') );
 
