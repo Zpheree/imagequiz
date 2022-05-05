e70cef921ad96d1823a673d0eade468b596151a1
habahram <habahram@email.arizona.edu>
2022-05-04 21:59:17 -0700

    login is working.

--- src/Communication/APIAccess.js
+++ src/Communication/APIAccess.js
@@ -1,4 +1,4 @@
-let backendAddress = 'https://zpheree-imagequiz-api.herokuapp.com';
+let backendAddress = 'http://localhost:4002'; //'https://zpheree-imagequiz-api.herokuapp.com'
 
 let apiAccess = {
     addCustomer: (name, email, password) => {
